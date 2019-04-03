b1f586574839637748295b95acb926da6b050ed2
DaeIn Lee <lazy0ren@gmail.com>
2019-04-03 23:36:42 +0900

    classification done

--- assignment2-decision_tree/dt.py
+++ assignment2-decision_tree/dt.py
@@ -5,29 +5,37 @@ import math
 import sys
 
 class Node:
-	def __init__(self, parent, attr, isLeaf=False):
+	def __init__(self, parent, attr, isLeaf=False, classLabel=""):
 		self.parent = parent
 		self.attr = attr
 		self.children = dict()
 		self.isLeaf = isLeaf
-		self.classLabel = ""
-		if parent == None:
-			self.level = 1
-		else:
-			self.level = parent.level + 1
+		self.classLabel = classLabel
 
 attrHeader = list()
 samples = list()
+attrValues = defaultdict(lambda: set())
 
-def loadData(fileName, header, rows):
+def loadData(fileName, header, rows, attrValues=None):
 	with open(fileName, 'r') as openedFile:
 		header.extend(openedFile.readline().rstrip('\n').split('\t'))
-		rows.extend(line.rstrip('\n').split('\t') for line in openedFile.readlines())
+		if attrValues is not None:
+			headerLength = len(header)
+			for line in openedFile.readlines():
+				line = line.rstrip('\n').split('\t')
+				rows.extend([line])
+				for col in range(headerLength):
+					attrValues[header[col]].add(line[col])
+			print(rows)
+		else:
+			rows.extend(line.rstrip('\n').split('\t') for line in openedFile.readlines())
+			print(rows)
+
+
 
 def calcEntropy(classified):
 	ret = 0.0
 	for val in classified:
-		# print("val", val)
 		p = val / sum(classified)
 		ret -= p * math.log(p, 2)
 	return ret
@@ -43,61 +51,64 @@ def calcGains(rows):
 		for row in rows:
 			classCounter[row[col]][row[-1]] += 1
 		for classified in classCounter.values():
-			# print(sum(classified.values()), DBsize, calcEntropy(classified.values()))
 			entropy += sum(classified.values()) / DBsize * calcEntropy(classified.values())
-		# print(entropy)
 		infoGains.append(totalEntropy - entropy)
 	return infoGains
 
 def attributeSelection(rows):
 	infoGains = calcGains(rows)
-	print(infoGains, max(infoGains))
 	return infoGains.index(max(infoGains))
 
 def generateTree(parent, attributes, dataPartitions):
 	classList = [row[-1] for row in dataPartitions]
 	classCounter = Counter(classList)
+	majorityVoted = classCounter.most_common(1)[0][0]
 	# 1: tuples are all of the same class
 	if (classCounter.most_common(1)[0][1] == len(classList)):
-		curNode = Node(parent, "", True)
-		curNode.classLabel = classCounter.most_common(1)[0][0]
-		return curNode
+		return Node(parent, "", True, majorityVoted)
 
 	# 2: attributes lits is empty MAJORITY VOTING
 	if len(attributes) == 0:
-		curNode = Node(parent, "", True)
-		curNode.classLabel = classCounter.most_common(1)[0][0]
-		return curNode
-	print(attributes)
+		return Node(parent, "", True, majorityVoted)
+
+	del classList
+	del classCounter
 	selectedAttrIdx = attributeSelection(dataPartitions)
-	# print(attributes)
 	curNode = Node(parent, attributes[selectedAttrIdx])
 	del attributes[selectedAttrIdx]
 	partition = defaultdict(lambda: [])
-	for idx, row in enumerate(dataPartitions):
-		partition[row[selectedAttrIdx]].append(row)
+	for row in dataPartitions:
+		attrValue = row[selectedAttrIdx]
+		del row[selectedAttrIdx]
+		partition[attrValue].append(row)
 	dataPartitions.clear()
-	for item in partition.items():
-		curNode.children[item[0]] = generateTree(curNode, attributes, item[1])
+	for attrValue in attrValues[curNode.attr]:
+		if not partition[attrValue]:# partition is empty
+			curNode.children[attrValue] = Node(curNode, "", True, majorityVoted)
+		else:
+			curNode.children[attrValue] = generateTree(curNode, attributes, partition[attrValue])
+	# for item in partition.items():
+	# 	curNode.children[item[0]] = generateTree(curNode, attributes, item[1])
 	attributes.insert(selectedAttrIdx, curNode.attr)
-	# print(attributes)
 	return curNode
 
 def _classification(node, attributes, sample):
 	if node.isLeaf:
 		return node.classLabel
-
+	print(node.attr)
+	print(attributes, attributes.index(node.attr), sample[attributes.index(node.attr)])
+	print(node.children)
+	return _classification(node.children[sample[attributes.index(node.attr)]], attributes, sample)
 
 def classification(testFile, resultFile, tree):
 	testHeader = list()
 	testSamples = list()
 	loadData(testFile, testHeader, testSamples)
-    with open(resultFile, 'w') as outFile:
-		outFile.write('\t'.join(attrHeader))
-        for sample in testSamples:
-            #찾아낸 class를 바탕으로 정답을 출력한다.
-            result = _classification(tree, testHeader, sample)
-			outFile.write('\t'.join(sample) + '\t' + result)
+	with open(resultFile, 'w') as outFile:
+		outFile.write('\t'.join(attrHeader) + '\n')
+		for sample in testSamples:
+			result = _classification(tree, testHeader, sample)
+			outFile.write('\t'.join(sample) + '\t' + result + '\n')
 
 if __name__ == "__main__":
 	if len(sys.argv) != 4:
@@ -105,9 +116,6 @@ if __name__ == "__main__":
 		print("***** USAGE *****")
 		print("%s [train file] [test file] [result file]" % sys.argv[0])
 		sys.exit("argv error")
-
-	loadData(sys.argv[1], attrHeader, samples)
-	# print(attrHeader)
-	# print(samples)
+	loadData(sys.argv[1], attrHeader, samples, attrValues)
 	decisionTree = generateTree(None, attrHeader, samples)
-	classification(sys.argv[2], sys.argv[3], tree)
+	classification(sys.argv[2], sys.argv[3], decisionTree)
